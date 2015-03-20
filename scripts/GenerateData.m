1f804551dcb8a50d111a936085e7b2b6c0aadbd9
MonkeyRum <monkey.rum.github@gmail.com>
2015-03-20 23:43:50 +0000

    More data generated
    And also create file csvs

--- scripts/GenerateData.m
+++ scripts/GenerateData.m
@@ -5,6 +5,10 @@ numFiles = size(filenames);
 numFiles = numFiles(2);
 
 funs = {
+            @(block_struct) graycoprops_fun(block_struct.data, 'Contrast'), ...
+            @(block_struct) graycoprops_fun(block_struct.data, 'Correlation'), ...
+            @(block_struct) graycoprops_fun(block_struct.data, 'Energy'), ...
+            @(block_struct) graycoprops_fun(block_struct.data, 'Homogeneity'), ...
             @(block_struct) mean2(block_struct.data), ...
             @(block_struct) median(median(block_struct.data)), ...
             @(block_struct) std(std(block_struct.data)), ...
@@ -16,6 +20,9 @@ funs = {
 numFuns = size(funs);
 numFuns = numFuns(2);
 
+regex = ',';
+replace = '.';
+
 for i=1:numFiles
     
     name = filenames{1, i};
@@ -23,20 +30,32 @@ for i=1:numFiles
     IR = I(:,:,1);
     IG = I(:,:,2);
     IB = I(:,:,3);
+    IGray = rgb2gray(I);
     
     for j=1:numFuns
         
         fun = funs{j};
         filename = filenames{i};
-       
+        
+        outFile = strcat(outDir, filename, '.', 'RED.', func2str(fun), '.csv');
+        outFile = regexprep(outFile,regex,replace);
         blocks = blockproc(double(IR), [w, h], fun);
-        csvwrite(strcat(outDir, filename, '.', 'RED.', func2str(fun), '.csv'), blocks);
+        csvwrite(outFile, blocks);
         
+        outFile = strcat(outDir, filename, '.', 'GREEN.', func2str(fun), '.csv');
+        outFile = regexprep(outFile,regex,replace);
         blocks = blockproc(double(IG), [w, h], fun);
-        csvwrite(strcat(outDir, filename, '.', 'GREEN.', func2str(fun), '.csv'), blocks);
+        csvwrite(outFile, blocks);
         
+        outFile = strcat(outDir, filename, '.', 'BLUE.', func2str(fun), '.csv');
+        outFile = regexprep(outFile,regex,replace);
         blocks = blockproc(double(IB), [w, h], fun);
-        csvwrite(strcat(outDir, filename, '.', 'BLUE.', func2str(fun), '.csv'), blocks);
+        csvwrite(outFile, blocks);
+        
+        outFile = strcat(outDir, filename, '.', 'GRAY.', func2str(fun), '.csv');
+        outFile = regexprep(outFile,regex,replace);
+        blocks = blockproc(double(IGray), [w, h], fun);
+        csvwrite(outFile, blocks);
         
     end
     
@@ -44,3 +63,9 @@ end
 
 end
 
+function [out_val] = graycoprops_fun (block, param)
+
+    out_val = graycoprops(block, param);
+    out_val = getfield(out_val, param);
+    
+end
--- vs/Set/Program.cs
+++ vs/Set/Program.cs
@@ -53,6 +53,11 @@ namespace Set
 
             WriteCsv(outputFolder + @"\training.csv", ref trainingCsv);
             WriteCsv(outputFolder + @"\testing.csv", ref testingCsv);
+
+            BuildLabelTestCsv(testingCsv);
+            BuildLabelTrainCsv(trainingCsv);
+            BuildDiscrimTestCsv(testingCsv);
+            BuildDiscrimTrainCsv(trainingCsv);
         }
 
         private static void SplitIntoSets(List<string> paths, ref string trainingCsv, ref string testingCsv, string suffix, string imageType)
@@ -73,6 +78,150 @@ namespace Set
                 {
                     testingCsv += csvEntry;
                 }
+            }
+        }
+
+        private static void BuildLabelTestCsv(string testing_csv)
+        {
+            string label_path = @"C:\Users\Alex\Dropbox\CMP3060M\ImageDataSet\ManualClassification\Alex2";
+
+            string[] files = testing_csv.Split(',');
+            RemoveExtensions(ref files);
+
+            string[] filePaths = Directory.GetFiles(label_path);
+            List<string> acceptedFilesPaths = new List<string>();
+
+            foreach (var str in filePaths)
+            {
+                foreach(var str2 in files)
+                {
+                    if(str.Contains(str2))
+                    {
+                        acceptedFilesPaths.Add(Path.GetFileName(str));
+                    }
+                }
+            }
+
+            acceptedFilesPaths = acceptedFilesPaths.Distinct().ToList();
+            string csvOut = string.Empty;
+
+            foreach(var str in acceptedFilesPaths)
+            {
+                csvOut += str + ",";
+            }
+
+            csvOut = csvOut.Substring(0, csvOut.Length - 1);
+
+            WriteCsv(@"C:\Users\Alex\Dropbox\CMP3060M\ImageDataSet\WORKING_SET\testing_labels.csv", ref csvOut);
+        }
+
+        private static void BuildLabelTrainCsv(string training_csv)
+        {
+            string label_path = @"C:\Users\Alex\Dropbox\CMP3060M\ImageDataSet\ManualClassification\Alex2";
+
+            string[] files = training_csv.Split(',');
+            RemoveExtensions(ref files);
+
+            string[] filePaths = Directory.GetFiles(label_path);
+            List<string> acceptedFilesPaths = new List<string>();
+
+            foreach (var str in filePaths)
+            {
+                foreach (var str2 in files)
+                {
+                    if (str.Contains(str2))
+                    {
+                        acceptedFilesPaths.Add(Path.GetFileName(str));
+                    }
+                }
+            }
+
+            acceptedFilesPaths = acceptedFilesPaths.Distinct().ToList();
+            string csvOut = string.Empty;
+
+            foreach (var str in acceptedFilesPaths)
+            {
+                csvOut += str + ",";
+            }
+
+            csvOut = csvOut.Substring(0, csvOut.Length - 1);
+
+            WriteCsv(@"C:\Users\Alex\Dropbox\CMP3060M\ImageDataSet\WORKING_SET\training_labels.csv", ref csvOut);
+        }
+
+        private static void BuildDiscrimTestCsv(string testing_csv)
+        {
+            string label_path = @"C:\Users\Alex\Dropbox\CMP3060M\ImageDataSet\FeatureData\256";
+
+            string[] files = testing_csv.Split(',');
+            RemoveExtensions(ref files);
+
+            string[] filePaths = Directory.GetFiles(label_path);
+            List<string> acceptedFilesPaths = new List<string>();
+
+            foreach (var str in filePaths)
+            {
+                foreach (var str2 in files)
+                {
+                    if (str.Contains(str2))
+                    {
+                        acceptedFilesPaths.Add(Path.GetFileName(str));
+                    }
+                }
+            }
+
+            acceptedFilesPaths = acceptedFilesPaths.Distinct().ToList();
+            string csvOut = string.Empty;
+
+            foreach (var str in acceptedFilesPaths)
+            {
+                csvOut += str + ",";
+            }
+
+            csvOut = csvOut.Substring(0, csvOut.Length - 1);
+
+            WriteCsv(@"C:\Users\Alex\Dropbox\CMP3060M\ImageDataSet\WORKING_SET\testing_data.csv", ref csvOut);
+        }
+
+        private static void BuildDiscrimTrainCsv(string training_csv)
+        {
+            string label_path = @"C:\Users\Alex\Dropbox\CMP3060M\ImageDataSet\FeatureData\256";
+
+            string[] files = training_csv.Split(',');
+            RemoveExtensions(ref files);
+
+            string[] filePaths = Directory.GetFiles(label_path);
+            List<string> acceptedFilesPaths = new List<string>();
+
+            foreach (var str in filePaths)
+            {
+                foreach (var str2 in files)
+                {
+                    if (str.Contains(str2))
+                    {
+                        acceptedFilesPaths.Add(Path.GetFileName(str));
+                    }
+                }
+            }
+
+            acceptedFilesPaths = acceptedFilesPaths.Distinct().ToList();
+            string csvOut = string.Empty;
+
+            foreach (var str in acceptedFilesPaths)
+            {
+                csvOut += str + ",";
+            }
+
+            csvOut = csvOut.Substring(0, csvOut.Length - 1);
+
+            WriteCsv(@"C:\Users\Alex\Dropbox\CMP3060M\ImageDataSet\WORKING_SET\training_data.csv", ref csvOut);
+        }
+
+        private static void RemoveExtensions(ref string[] path)
+        {
+            for(int i = 0; i < path.Length; ++i)
+            {
+                path[i] = Path.GetFileNameWithoutExtension(path[i]);
             }
         }
 
